7510d1338efed6a4039348938a3cd20827c6bbab
Vadim Troshkov <tvaa@ya.ru>
2020-05-12 20:03:43 +0300

    120520

--- script/script.js
+++ script/script.js
@@ -1,53 +1,50 @@
 //Get operands 
 
 const content = document.querySelector('.content');
-const editButton = content.querySelector('.profile__button-edit'); 
+const editButton = content.querySelector('.profile__button-edit');
 const closeButton = content.querySelector('.popup-container__button-close');
-const popupEdit = content.querySelector('.popup__edit');
+const popupEdit = content.querySelector('.popup');
 
-let authorInput = content.querySelector('.popup-container__infoform_author'); 
-let aboutInput = content.querySelector('.popup-container__infoform_about'); 
-let profileAuthor = content.querySelector('.profile__author-name'); 
-let profileAbout = content.querySelector('.profile__about'); 
-
-
-
-//Define functions
-function openForm() {
-    const newClass= 'popup_opened';
-    popupEdit.classList.add(newClass);
-
-  }
+let authorInput = content.querySelector('.popup-container__infoform_author');
+let aboutInput = content.querySelector('.popup-container__infoform_about');
+let profileAuthor = content.querySelector('.profile__author-name');
+let profileAbout = content.querySelector('.profile__about');
 
 
-function editForm () {
-    openForm(popupEdit);
-  
-    authorInput.value = profileAuthor.textContent;
-    aboutInput.value = profileAbout.textContent;
 
-  }
- 
- function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-
-    profileAuthor.textContent = authorInput.value;
-    profileAbout.textContent = aboutInput.value; 
-
-    closeForm();
-}
-
-function closeForm () {
-
-  const newClass= 'popup_opened';
-  popupEdit.classList.toggle(newClass);
-  if (popupEdit.classList.contains(newClass)) {
-    popupEdit.classList.remove(newClass);  
-  }
+function editForm() {
+  //Fill the fields from the page to the form
+  authorInput.value = profileAuthor.textContent;
+  aboutInput.value = profileAbout.textContent;
+  //Change the form with proper classList
+  togglePopup(popupEdit);
 };
 
+function togglePopup(popupElement) {
+  popupElement.classList.toggle('popup_opened'); 
+};
+
+//Submit result
+function formSubmitHandler(evt) {
+
+  evt.preventDefault();
+
+  //Put inserted data to the form fields
+  profileAuthor.textContent = authorInput.value;
+  profileAbout.textContent = aboutInput.value;
+
+  //closeForm();
+  togglePopup(popupEdit);
+}
+
+
+
+
 //Add some events
+
+
 editButton.addEventListener('click', editForm);
-closeButton.addEventListener('click', closeForm);
+closeButton.addEventListener('click', editForm);
+
 popupEdit.addEventListener('submit', formSubmitHandler);
 
